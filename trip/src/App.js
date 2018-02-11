e232e32f250a13bd48d990b0476079b6d483ca93
Tom M <tmckinney121@gmail.com>
2018-02-10 23:16:24 -0500

    Added basic example of react-google-maps

--- trip/src/App.js
+++ trip/src/App.js
@@ -3,6 +3,7 @@ import logo from './usa_map.jpg';
 import './App.css';
 
 import Itinerary, { Test } from './itinerary/Itinerary';
+import Map from './map/Map';
 
 class App extends Component {
   render() {
@@ -16,6 +17,13 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Itinerary widget={"Test"} />
+        <Map
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+         />
       </div>
     );
   }
