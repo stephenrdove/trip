da2c5e27e0763e006b0186fee1222a02882b4ef8
Stephen Dove <stephendove@Stephen33Laptop.local>
2018-02-10 23:16:39 -0500

    added itin css class

--- trip/src/App.js
+++ trip/src/App.js
@@ -16,14 +16,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Itinerary widget={"Test"} />
-        <Map
-          isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-         />
+        <div className='itinerary-container'>
+          <div className='list'>
+            'THIS IS WHERE THE TEXT GOES'
+            <Itinerary widget={"Test"} />
+          </div>
+          <div className='map'>
+            <Map
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+          </div>
+        </div>
       </div>
     );
   }
